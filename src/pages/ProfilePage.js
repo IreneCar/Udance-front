c2460985d989e6416f6d6662db1cba4e827c56a7
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-07-24 13:32:48 +0200

    profile side css

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -3,6 +3,7 @@ import { getProfileService } from "../services/profile.services.js";
 import { Link, Outlet } from "react-router-dom";
 import profileImg from "../images/profile.jpg";
 import React from "react";
+import addIcon from "../images/addIcon-01.png";
 
 function ProfilePage(props) {
   const [profile, setProfile] = useState("profile");
@@ -27,12 +28,23 @@ function ProfilePage(props) {
         <img src={profileImg} alt="my-profile" className="circular-portrait" />
 
         <h1>{profile.name}</h1>
-        <Link className="navBarLink" to="/profile/given">
+        <p>This is the profile description</p>
+        <p>Styles I dance</p>
+
+        <Link className="link" to="/profile/given">
           Gived Lessons
         </Link>
-        <Link className="navBarLink" to="/profile/received">
+
+        <Link className="link" to="/profile/received">
           Received Lessons
         </Link>
+
+        <img
+          src={addIcon}
+          alt="icon to add clases"
+          width={"10%"}
+          style={{ marginTop: "10px" }}
+        />
       </div>
 
       <div>
