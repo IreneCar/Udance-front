cb1dc44ca135bf90b8ff07c3975f6a599c76c456
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-07-26 17:46:27 +0200

    Lesson details Page css done

--- src/pages/LessonDetailsPage.js
+++ src/pages/LessonDetailsPage.js
@@ -1,82 +1,118 @@
-import { useState, useEffect } from 'react';
-import { getLessonDetailsService } from '../services/lesson.services';
+import { useState, useEffect } from "react";
+import { getLessonDetailsService } from "../services/lesson.services";
 import { joinLessonService } from "../services/lesson.services.js";
 import { dropOffLessonService } from "../services/lesson.services.js";
-import { Link, useParams, useNavigate } from 'react-router-dom';
-
+import { Link, useParams, useNavigate } from "react-router-dom";
+import img14 from "../images/img14.jpg";
 
 function LessonDetailsPage(props) {
-	const navigate = useNavigate();
-	const [ lesson, setLesson ] = useState(null);
-	const { id } = useParams();
-	const projectId = id;
+  const navigate = useNavigate();
+  const [lesson, setLesson] = useState(null);
+  const { id } = useParams();
+  const projectId = id;
 
-	const getProject = async () => {
-		localStorage.getItem('authToken');
-		try {
-			const response = await getLessonDetailsService(id);
-			setLesson(response.data);
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  const getProject = async () => {
+    localStorage.getItem("authToken");
+    try {
+      const response = await getLessonDetailsService(id);
+      setLesson(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	useEffect(() => {
-		getProject();
-		// eslint-disable-next-line
-	}, []);
+  useEffect(() => {
+    getProject();
+    // eslint-disable-next-line
+  }, []);
 
+  const joinLesson = async () => {
+    try {
+      await joinLessonService(projectId);
+      navigate("/profile");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  const dropOffLesson = async () => {
+    try {
+      await dropOffLessonService(projectId);
+      navigate("/profile");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	const joinLesson  = async () => {
-		try {
-			await joinLessonService (projectId);
-		navigate('/profile');
-	  }
-	  catch (err) {
-		console.log(err);
-	  }
-	}
+  return (
+    <div className="LessonDetails">
+      {!lesson && <h3>Lesson not found!</h3>}
+      {lesson && (
+        <div>
+          <img src={img14} alt="dancing-lesson" />
+          {/* <p>Image: {lesson.image}</p> */}
+          <h1>{lesson.title}</h1>
+          <div className="LessonDetails-wrap-row2">
+            <h5>Teacher:</h5>
+            <p>{lesson.teacher}</p>
+          </div>
+          <div className="LessonDetails-wrap-row2">
+            <h5>Cohosts:</h5>
+            <p>{lesson.cohost}</p>
+          </div>
+          <div className="LessonDetails-wrap-row2">
+            <h5>Styles:</h5>
+            <p>{lesson.styles}</p>
+          </div>
 
-	const dropOffLesson  = async () => {
-		try {
-			await dropOffLessonService (projectId);
-		navigate('/profile');
-	  }
-	  catch (err) {
-		console.log(err);
-	  }
-	}
+          <div className="LessonDetails-space">
+            <div className="LessonDetails-wrap-row">
+              <div className="LessonDetails-wrap-row2">
+                <h5>Location:</h5>
+                <p>{lesson.location}</p>
+              </div>
+              <div className="LessonDetails-wrap-row2">
+                <h5>Days:</h5>
+                <p>{lesson.days}</p>
+              </div>
+              <div className="LessonDetails-wrap-row2">
+                <h5>Hours:</h5>
+                <p>{lesson.hours}</p>
+              </div>
+            </div>
 
+            <div className="LessonDetails-wrap-row">
+              <div className="LessonDetails-wrap-row2">
+                <h5>First Day:</h5>
+                <p>{lesson.firstDay}</p>
+              </div>
+              <div className="LessonDetails-wrap-row2">
+                <h5>Last Day:</h5>
+                <p>{lesson.lastDay}</p>
+              </div>
+            </div>
+          </div>
 
-	return (
-		<div className="ProjectDetails">
-			{!lesson && <h3>Lesson not found!</h3>}
-			{lesson && (
-				<div>
-					
-					<h1>Lesson: {lesson.title}</h1>
-					<p>Teacher: {lesson.teacher}</p>
-					<p>Cohosts: {lesson.cohost}</p>
-					<p>Styles: {lesson.styles}</p>
-					<p>Location: {lesson.location}</p>
-					<p>Days: {lesson.days}</p>
-					<p>Hours: {lesson.hours}</p>
-					<p>First Day: {lesson.firstDay}</p>
-					<p>Last Day: {lesson.lastDay}</p>
-					<p>Price: {lesson.price}</p>
-					<p>Details: {lesson.details}</p>
-					<p>Image: {lesson.image}</p>
-					<button onClick={joinLesson}>Join Lesson</button>
-					<button onClick={dropOffLesson}>Drop Off Lesson</button>
+          <div className="LessonDetails-details">
+            <h5>Details:</h5>
+            <p>{lesson.details}</p>
+          </div>
+          <div>
+            <h5>Price:</h5>
+            <p style={{ fontWeight: "bold", fontSize: "20px" }}>
+              {lesson.price} €
+            </p>
+          </div>
 
-				</div>
-			)}
-			<Link to="/lessons">
-				<button>Back to lessons</button>
-			</Link>
-		</div>
-	);
+          <button onClick={joinLesson}>Join Lesson</button>
+          <button onClick={dropOffLesson}>Drop Off Lesson</button>
+        </div>
+      )}
+      <Link to="/lessons">
+        <button>Back to lessons</button>
+      </Link>
+    </div>
+  );
 }
 
 export default LessonDetailsPage;
