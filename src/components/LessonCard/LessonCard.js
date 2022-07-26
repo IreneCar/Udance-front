8de70f7d66510adad96f1fdbff1790523c9b2878
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-07-26 13:00:12 +0200

    al forms and cards css done

--- src/components/LessonCard/LessonCard.js
+++ src/components/LessonCard/LessonCard.js
@@ -5,26 +5,32 @@ import "./LessonCard.css";
 function ProjectCard({ title, lastDay, firstDay, _id, teacher }) {
   return (
     <div className="LessonCard">
-      <div className="circular-portrait ">
+      <div className="circular-portrait" style={{ margin: "0px" }}>
         <img
           src="https://www.kindpng.com/picc/m/353-3534825_cool-profile-avatar-picture-cool-profile-hd-png.png"
           alt="teacher"
         />
       </div>
-      <div>
+      <div className="LessonCard-wrapInfo">
         <h3>{title}</h3>
 
-        <div className="LessonCard-date ">
+        <div className="LessonCard-info">
           <span>{teacher}</span>
 
-          <div>
+          <div className="LessonCard-date">
             <p>{firstDay}</p>
+            <p>-</p>
             <p>{lastDay}</p>
           </div>
         </div>
-        <br />
-        <Link to={`/lessons/${_id}`}>
-          <h3>View more details</h3>
+
+        <Link
+          to={`/lessons/${_id}`}
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <button>View more details</button>
         </Link>
       </div>
     </div>
