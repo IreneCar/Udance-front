136136caf9a20bc2e3a53e9e9c9c0f76a1f4ee88
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-07-27 15:10:34 +0200

    Given card added

---
+++ src/components/GivenCard/GivenCard.js
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import "./GivenCard.css";
+
+// We are deconstructing props object directly in the parentheses of the function
+function GivenCard({ title, lastDay, firstDay, _id, teacher }) {
+  return (
+    <div className="GivenCard">
+      <div className="circular-portrait" style={{ margin: "0px" }}>
+        <img
+          src="https://www.kindpng.com/picc/m/353-3534825_cool-profile-avatar-picture-cool-profile-hd-png.png"
+          alt="teacher"
+        />
+      </div>
+      <div className="GivenCard-wrapInfo">
+        <h3>{title}</h3>
+
+        <div className="GivenCard-info">
+          <h6>{teacher}</h6>
+
+          <div className="GivenCard-date">
+            <p>{firstDay}</p>
+            <p>/</p>
+            <p>{lastDay}</p>
+          </div>
+        </div>
+
+        <Link to={"#"} className="GivenCard-Link">
+          Send mail
+        </Link>
+        <Link
+          to={`/lessons/${_id}`}
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <button>View more details</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default GivenCard;
--- src/pages/GivLessonsPage.js
+++ src/pages/GivLessonsPage.js
@@ -1,36 +1,38 @@
-import { useState, useEffect } from 'react';
-import LessonCard from '../components/LessonCard/LessonCard';
-import { getGivedLessonsService } from '../services/profile.services';
+import { useState, useEffect } from "react";
+import LessonCard from "../components/LessonCard/LessonCard";
+import GivenCard from "../components/GivenCard/GivenCard";
+import { getGivedLessonsService } from "../services/profile.services";
 
 function GivLessonPage() {
-	const [ givenLessons, setGivenLessons] = useState([]);
-	const [ loading, setLoading ] = useState(true);
+  const [givenLessons, setGivenLessons] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-	const getGivenLessons = async () => {
-		// Send the token through the request "Authorization" Headers
-		try {
-			const response = await getGivedLessonsService();
-			setGivenLessons(response.data);
-			setLoading(false);
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  const getGivenLessons = async () => {
+    // Send the token through the request "Authorization" Headers
+    try {
+      const response = await getGivedLessonsService();
+      setGivenLessons(response.data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	// We set this effect will run only once, after the initial render
-	// by setting the empty dependency array - []
-	useEffect(() => {
-		getGivenLessons();
-	}, []);
+  // We set this effect will run only once, after the initial render
+  // by setting the empty dependency array - []
+  useEffect(() => {
+    getGivenLessons();
+  }, []);
 
-	return (
-		<div className="ProjectListPage">
-		
-
-			{loading && <div>Loading...</div>}
-			 { !loading && givenLessons?.map((lesson) => <LessonCard key={lesson._id} {...lesson} />  )}  
-		</div>
-	);
+  return (
+    <div className="ProjectListPage">
+      {loading && <div>Loading...</div>}
+      {!loading &&
+        givenLessons?.map((lesson) => (
+          <GivenCard key={lesson._id} {...lesson} />
+        ))}
+    </div>
+  );
 }
 
 export default GivLessonPage;
