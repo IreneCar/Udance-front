031360f1bdb5a5362e7d2804bba7d0c1211ba9d2
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-07-27 15:10:13 +0200

    Given lessons card created
    Adding send email link

--- src/pages/RecLessonsPage.js
+++ src/pages/RecLessonsPage.js
@@ -1,36 +1,37 @@
-import { useState, useEffect } from 'react';
-import LessonCard from '../components/LessonCard/LessonCard';
-import { getReceivedLessonsService } from '../services/profile.services';
+import { useState, useEffect } from "react";
+import LessonCard from "../components/LessonCard/LessonCard";
+import { getReceivedLessonsService } from "../services/profile.services";
 
 function RecLessonPage() {
-	const [ receivedLessons, setReceivedLessons] = useState([]);
-	const [ loading, setLoading ] = useState(true);
+  const [receivedLessons, setReceivedLessons] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-	const getReceivedLessons = async () => {
-		// Send the token through the request "Authorization" Headers
-		try {
-			const response = await getReceivedLessonsService();
-			setReceivedLessons(response.data);
-			setLoading(false);
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  const getReceivedLessons = async () => {
+    // Send the token through the request "Authorization" Headers
+    try {
+      const response = await getReceivedLessonsService();
+      setReceivedLessons(response.data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	// We set this effect will run only once, after the initial render
-	// by setting the empty dependency array - []
-	useEffect(() => {
-		getReceivedLessons();
-	}, []);
+  // We set this effect will run only once, after the initial render
+  // by setting the empty dependency array - []
+  useEffect(() => {
+    getReceivedLessons();
+  }, []);
 
-	return (
-		<div className="ProjectListPage">
-		
-
-			{loading && <div>Loading...</div>}
-			 { !loading && receivedLessons?.map((lesson) => <LessonCard key={lesson._id} {...lesson} />  )}  
-		</div>
-	);
+  return (
+    <div className="ProjectListPage">
+      {loading && <div>Loading...</div>}
+      {!loading &&
+        receivedLessons?.map((lesson) => (
+          <LessonCard key={lesson._id} {...lesson} />
+        ))}
+    </div>
+  );
 }
 
 export default RecLessonPage;
