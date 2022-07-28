2eae3183bc5c62703959134f8d8f1049faa014d2
RubenGH88 <104738166+RubenGH88@users.noreply.github.com>
2022-07-28 11:49:35 +0200

    date implemented
    date implemented

--- src/components/AddLesson.js
+++ src/components/AddLesson.js
@@ -15,7 +15,13 @@ function AddLesson(props) {
   const [price, setPrice] = useState("");
   const [details, setDetails] = useState("");
   const [cohost, setCohost] = useState("");
-  const [image, setImage] = useState("");
+
+
+  var date = new Date(Date.now()); // Date 2011-05-09T06:08:45.178Z
+  var year = date.getFullYear();
+  var month = ("0" + (date.getMonth() + 1)).slice(-2);
+  var day = ("0" + date.getDate()).slice(-2);
+  var today=`${year}-${month}-${day}`
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -31,7 +37,7 @@ function AddLesson(props) {
       price,
       details,
       cohost,
-      image,
+     
     };
 
     // Send the token through the request "Authorization" Headers
@@ -48,7 +54,7 @@ function AddLesson(props) {
       setPrice("");
       setDetails("");
       setCohost("");
-      setImage("");
+     
       props.refreshProjects();
     } catch (err) {
       console.log(err);
@@ -102,16 +108,19 @@ function AddLesson(props) {
 
         <label>First Day:</label>
         <input
-          type="text"
+          type="date"
           name="first-day"
+          min={today}
+          max={lastDay}
           value={firstDay}
           onChange={(e) => setFirstDay(e.target.value)}
         />
 
         <label>Last Day:</label>
         <input
-          type="text"
+          type="date"
           name="last-day"
+          min={firstDay}
           value={lastDay}
           onChange={(e) => setLastDay(e.target.value)}
         />
@@ -140,13 +149,7 @@ function AddLesson(props) {
           onChange={(e) => setCohost(e.target.value)}
         />
 
-        <label>Image:</label>
-        <input
-          type="text"
-          name="image"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-        />
+   
 
         <button type="submit">Submit</button>
       </form>
